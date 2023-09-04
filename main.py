35fdb9f7bac766cd1669cdaf284e9e449e62fea1
KirillMelanich <kirillmelanich@gmail.com>
2023-09-04 14:07:32 +0300

    first code

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
+import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import httpx
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+URL = "https://www.google.com/"
+
+
+def send_request(num: int, url: str):
+    print(f'Sending request #{num}')
+    response = httpx.get(url)
+    print(response.status_code)
+
+
+def main_sync():
+    for i in range(10):
+        send_request(i, URL)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    start = time.perf_counter()
+    main_sync()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    end = time.perf_counter()
+
+    print("Elapsed:", end - start)
+
